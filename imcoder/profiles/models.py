beab30abb49a6aea43e1635412bdf57f854c3c97
Erik Rivera <erik.river@gmail.com>
2012-09-29 18:03:37 -0500

    modelo

--- imcoder/profiles/models.py
+++ imcoder/profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tagging.fields import TagField
 
-# Create your models here.
+STATUS = ((1,'Disponible'),
+        (2,'No Disponible'),
+        (3, 'Freelance'),
+)
+
+class Coder(models.Model):
+    user = models.OneToOneField(User)
+    languages = TagField()
+    status = models.CharField(max_length=20, choices=STATUS)
+    recommended = models.ManyToManyField(User)
+    friends = models.ManyToManyField(User)
+
